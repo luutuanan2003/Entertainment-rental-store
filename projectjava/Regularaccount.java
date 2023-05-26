1a209eedaff38e769b13e4b583f886b1c0543107
darmond0909 <132368628+darmond0909@users.noreply.github.com>
2023-05-26 20:21:03 +0700

    Update Regularaccount.java

--- projectjava/Regularaccount.java
+++ projectjava/Regularaccount.java
@@ -1,17 +1,19 @@
-package projectjava;
-
 import java.util.ArrayList;
 
 public class Regularaccount extends Customer{
-    public Regularaccount(String ID, String name, int phone, ArrayList<Item> items, String username, String password) {
+
+    public Regularaccount(String ID, String name, String phone, ArrayList<Item> items, String username, String password) {
         super(ID, name, phone, items, username, password);
     }
     @Override
     public void returned(){
+        super.returned();
 
     }
     @Override
-    public void rent(Item items) {
-
+    public String rent()
+    {
+       String customerID = super.rent();
+       return customerID;
     }
 }
