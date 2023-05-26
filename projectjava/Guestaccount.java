7ab22c2aba6938213a39e823e54885753b2aa7bb
luandang2112 <132368857+luandang2112@users.noreply.github.com>
2023-05-26 22:36:36 +0700

    Update Guestaccount.java

--- projectjava/Guestaccount.java
+++ projectjava/Guestaccount.java
@@ -4,8 +4,8 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class Guestaccount extends Customer{
-    public Guestaccount(String ID, String name, String phone, ArrayList<Item> items, String username, String password) {
-        super(ID, name, phone, items, username, password);
+    public Guestaccount(String ID, String name, String address ,String phone, int total_rentals, String customerType, String username, String password) {
+        super(ID,  name,  address , phone,  total_rentals, customerType,  username, password);
     }
 
 
