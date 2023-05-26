ae354639c5848c5361b5367872223a5f59d97698
darmond0909 <132368628+darmond0909@users.noreply.github.com>
2023-05-26 22:39:26 +0700

    Update VIPaccount.java

--- projectjava/VIPaccount.java
+++ projectjava/VIPaccount.java
@@ -1,17 +1,44 @@
-package projectjava;
+import java.io.*;
 
-import java.util.ArrayList;
 
 public class VIPaccount extends Customer{
-    private int rewardpoints;
 
-    public VIPaccount(String ID, String name, String address ,String phone, int total_rentals, String customerType, String username, String password, int rewardpoints) {
-        super(ID,  name,  address , phone,  total_rentals, customerType,  username, password);
-        this.rewardpoints = rewardpoints;
+
+    public VIPaccount(String ID, String name, String address ,String phone, int total_rentals, String customerType, String username, String password) {
+        super(ID, name, address,phone, total_rentals, customerType, username, password);
     }
 
-    public int getRewardpoints() {
-        return rewardpoints;
+    public void addRewardPoint(String CID) {
+        try {
+            File inputFile = new File("managementCus.txt");
+            File tempFile = new File("tempManagementCus.txt");
+
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] customerInfo = line.split(",");
+                if (customerInfo[0].equals(CID)) {
+                    int rewardPoints = Integer.parseInt(customerInfo[5]);
+                    rewardPoints += 10; // Add 10 to the current reward points
+                    customerInfo[5] = String.valueOf(rewardPoints);
+                    line = String.join(",", customerInfo);
+                }
+                writer.write(line);
+                writer.newLine();
+            }
+
+            reader.close();
+            writer.close();
+
+            inputFile.delete();
+            tempFile.renameTo(inputFile);
+
+            System.out.println("Reward points for customer " + CID + " have been updated in the management file.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -22,6 +49,7 @@ public class VIPaccount extends Customer{
     }
 
     public void Vreturned(){
-        String cID = super.returned();
+        String CID = super.returned();
+        addRewardPoint(CID);
     }
 }
