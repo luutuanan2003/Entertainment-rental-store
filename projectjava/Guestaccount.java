358e73e165772d26c8b3dad0f6a608989c213beb
darmond0909 <132368628+darmond0909@users.noreply.github.com>
2023-05-26 23:47:17 +0700

    Update Guestaccount.java

--- projectjava/Guestaccount.java
+++ projectjava/Guestaccount.java
@@ -1,7 +1,6 @@
-package projectjava;
-
 import java.io.*;
-import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class Guestaccount extends Customer{
     public Guestaccount(String ID, String name, String address ,String phone, int total_rentals, String customerType, String username, String password) {
@@ -60,15 +59,128 @@ public class Guestaccount extends Customer{
     }
 
     public void Greturned(){
+
         String cID = super.returned();
     }
-    @Override
-    public String rent()
+
+    public String Grent()
     {
-        String customerID = super.rent();
+        Scanner scanner = new Scanner(System.in);
+        String customerID = "";
+
+        boolean validCustomerID = false;
+        while (!validCustomerID) {
+            System.out.println("Please enter your customer ID: ");
+            customerID = scanner.nextLine().trim();
+
+            if (!customerID.matches("C\\d{3}")) {
+                System.out.println("Invalid customer ID format. Please provide a valid customer ID.");
+            } else {
+                boolean customerFound = false;
+
+                try (BufferedReader br = new BufferedReader(new FileReader("customers.txt"))) {
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        String[] customerInfo = line.split(",");
+                        if (customerInfo[0].equals(customerID)) {
+                            customerFound = true;
+                            break;
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if (customerFound) {
+                    validCustomerID = true;
+                } else {
+                    System.out.println("Customer not found with ID: " + customerID);
+                }
+            }
+        }
+        String accountType = getAccountType(customerID);
+        int numberofRentals = getTotalRentalItems(customerID);
+        if (accountType.equals("Guest") && numberofRentals == 2)
+        {
+            System.out.println("You have reached your maximum rental items.");
+        }
+        else {
+            System.out.println("Customer ID verified");
+            System.out.println("------------");
+
+            // Prompt user for item ID
+            String itemID = "";
+            boolean validItemID = false;
+            boolean itemFound = false; // Flag to indicate if the item with the provided ID is found
+            while (!validItemID || !itemFound) {
+                System.out.println("Please enter the item ID: ");
+                if (scanner.hasNextLine()) {
+                    itemID = scanner.nextLine().trim(); // Trim any leading/trailing whitespace
+
+                    // Check if the item ID is valid
+                    if (!itemID.matches("I\\d{3}-\\d{4}(,.+)?")) {
+                        System.out.println("Invalid item ID format. Please provide a valid item ID.");
+                        scanner.skip(".+"); // Clear the buffer
+                        validItemID = false;
+                    } else {
+                        validItemID = true;
+                    }
+                    String loanType = getLoanType(itemID);
+
+                    if (accountType.equals("Guest") && loanType.equals("2-day")) {
+                        System.out.println("You are not allowed to rent this kind of item");
+                        return customerID; // Exit the method
+                    } else {
+                        // Retrieve information about the item from the items.txt file
+                        try (BufferedReader br = new BufferedReader(new FileReader("items.txt"))) {
+                            String line;
+                            while ((line = br.readLine()) != null) {
+                                String[] itemInfo = line.split(",");
+                                if (itemInfo[0].equals(itemID)) {
+                                    System.out.println("------------");
+                                    System.out.println("Item ID: " + itemInfo[0]);
+                                    System.out.println("Item Name: " + itemInfo[1]);
+                                    System.out.println("Item Price: " + itemInfo[5]);
+
+                                    // Store the customer ID, item ID, and quantity in the orders array
+                                    String[][] orders = new String[1][2];
+                                    orders[0][0] = customerID;
+                                    orders[0][1] = itemID;
+                                    itemFound = true;
+                                    break;
+                                }
+                            }
+
+                            if (!itemFound) {
+                                System.out.println("Item not found with ID: " + itemID);
+                                validItemID = false;
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+                // Print the order details
+                System.out.println("------------");
+                System.out.println("Order Details: ");
+                System.out.println("Customer ID: " + customerID);
+                System.out.println("Item ID: " + itemID);
+
+                // Save the order details to the orders.txt file
+                try (BufferedWriter bw = new BufferedWriter(new FileWriter("rentorders.txt", true))) {
+                    bw.write(customerID + "," + itemID);
+                    bw.newLine();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                scanner.close(); // Close the scanner
+
+            }
+        }
         return customerID;
     }
 
 }
-
 
