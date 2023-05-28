abc0342b5cf963f64d52f1ad19cd6c9838696419
luutuanan2003 <132368655+luutuanan2003@users.noreply.github.com>
2023-05-28 19:35:16 +0700

    Delete projectjava directory

--- projectjava/Videogame.java
+++
@@ -1,7 +0,0 @@
-package projectjava;
-
-public class Videogame extends Item{
-    public Videogame(String ID, String title, String rentalType, int quantity, int rentalFee, boolean rentalStatus) {
-        super(ID, title, rentalType, quantity, rentalFee, rentalStatus);
-    }
-}
